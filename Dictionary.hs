a989e74f3323fa113eb32febdcc442b113270cfb
BLUE <danil228aeedj@mail.ru>
2024-11-09 15:19:42 +0300

    mend

--- Dictionary.hs
+++ Dictionary.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE FlexibleInstances #-}
-module Dictionary where 
--- Define the MyHashMap class with a class constraint on keys
+
+module Dictionary where
+
+-- Define the Dictionary class (interface) with a class constraint on keys
 class (Eq k) => Dictionary hm k v where
-    empty :: Int-> hm k v                  -- An empty hash map
-    insert :: k -> v -> hm k v  -> hm k v   -- Insert a key-value pair
-    lookup :: k -> hm k v  -> Maybe v -- Lookup a value by key
-    delete :: k -> hm k v  -> hm k v   -- Delete a key-value pair
-    len :: hm k v -> Int -- return the amount of buckets
-    createMap :: Int -> [(k,v)] ->hm k v -- create map from list of pairs
+  empty :: Int -> hm k v -- Create An empty hash map
+  insert :: k -> v -> hm k v -> hm k v -- Insert a key-value pair
+  lookup :: k -> hm k v -> Maybe v -- Lookup a value by key
+  delete :: k -> hm k v -> hm k v -- Delete a key-value pair
+  len :: hm k v -> Int -- Return the amount of buckets
+  createMap :: Int -> [(k, v)] -> hm k v -- Create map from list of pairs
+  isKeyExist :: k -> hm k v -> Bool -- Check that key is inside map
+  isValueExist :: k -> v -> hm k v -> Bool -- Check that value is inside map
--- HashMap.hs
+++ HashMap.hs
@@ -1,15 +1,17 @@
+module HashMap where
 
-module HashMap  where
-import Dictionary (Dictionary(..))
-import Functions (customFilter, customFoldl, customFoldr, customMap)
 import Data.Array
 import Data.Hashable
+import Dictionary (Dictionary (..))
+import Functions (customFilter, customFoldl, customFoldr, customMap)
 
-
-data HashMap k v = HashMap {
-    buckets :: Array Int [(k, v)],  -- Each bucket holds a list of key-value pairs
-    size :: Int                     -- Number of buckets
-} | Nil deriving (Show)
+data HashMap k v
+  = HashMap
+      { buckets :: Array Int [(k, v)], -- Each bucket holds a list of key-value pairs
+        size :: Int -- Number of buckets
+      }
+  | Nil
+  deriving (Show)
 
 -- Helper functions
 bucketIndex :: (Hashable k) => HashMap k v -> k -> Int
@@ -18,32 +20,33 @@ bucketIndex hashmap key = hash key `mod` size hashmap
 lookupHelper :: (Eq k) => k -> [(k, v)] -> Maybe v
 lookupHelper key = customFoldr (\(k, v) acc -> if k == key then Just v else acc) Nothing
 
-instance (Hashable k) => Dictionary HashMap k v  where
-    empty s | s >= 0 =HashMap (array (0, s) [(i, []) | i <- [0..s]]) s
-            | otherwise = Nil
+instance (Hashable k) => Dictionary HashMap k v where
+  empty s
+    | s >= 0 = HashMap (array (0, s) [(i, []) | i <- [0 .. s]]) s
+    | otherwise = Nil
 
-    insert key value hashmap =
-        let index = bucketIndex hashmap key
-            oldBucket = buckets hashmap ! index
-            newBucket = (key, value) : customFilter (\(k, _) -> k /= key) oldBucket
-            newBuckets = buckets hashmap // [(index, newBucket)]
-        in hashmap { buckets = newBuckets }       
+  insert key value hashmap =
+    let index = bucketIndex hashmap key
+        oldBucket = buckets hashmap ! index
+        newBucket = (key, value) : customFilter (\(k, _) -> k /= key) oldBucket
+        newBuckets = buckets hashmap // [(index, newBucket)]
+     in hashmap {buckets = newBuckets}
 
-    lookup key hashmap =
-        let index = bucketIndex hashmap key
-            bucket = buckets hashmap ! index
-        in lookupHelper key bucket     
-    
-    delete key hashmap =
-        let index = bucketIndex hashmap key
-            oldBucket = buckets hashmap ! index
-            newBucket = customFilter (\(k, _) -> k /= key) oldBucket
-            newBuckets = buckets hashmap // [(index, newBucket)]
-        in hashmap { buckets = newBuckets }
+  lookup key hashmap =
+    let index = bucketIndex hashmap key
+        bucket = buckets hashmap ! index
+     in lookupHelper key bucket
 
-    len hashmap = size hashmap
+  delete key hashmap =
+    let index = bucketIndex hashmap key
+        oldBucket = buckets hashmap ! index
+        newBucket = customFilter (\(k, _) -> k /= key) oldBucket
+        newBuckets = buckets hashmap // [(index, newBucket)]
+     in hashmap {buckets = newBuckets}
 
-    createMap s [] = empty s
-    createMap s xs = 
-        let hashTable = empty s
-        in customFoldl (\(k,v) acc -> insert k v acc) hashTable xs
+  len hashmap = size hashmap
+
+  createMap s [] = empty s
+  createMap s xs =
+    let hashTable = empty s
+     in customFoldl (\(k, v) acc -> insert k v acc) hashTable xs
